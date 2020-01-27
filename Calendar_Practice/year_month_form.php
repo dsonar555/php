1d3fa5a02e428e12bed51e9147bdc93a1ec3e863
Divya Sonar <divyasonar555@gmail.com>
2020-01-27 15:03:52 +0530

    calendar with file upload

--- Calendar_Practice/year_month_form.php
+++ Calendar_Practice/year_month_form.php
@@ -3,6 +3,11 @@ session_start();
 if(isset($_POST['showCalendar'])) {
     $year = $_POST['year'];
     $month = $_POST['month'];
+    $file = $_FILES['image'];
+    if(uploadImage($file)) {
+        $_SESSION['image'] = $file['name'];
+        $_SESSION['path'] = 'uploads/';
+    }
     if(!empty($year) && !empty($month))
     {
         $monthValidation = ( ($month > 0) && ($month <= 12) && preg_match('/^\d{0,2}$/',$month) );
@@ -44,6 +49,31 @@ function setInterval($year, $month)
     $end = $end->modify("-1 day");
     return array($begin,$end,$interval,$daterange);
 }
+function uploadImage($file) {
+    $fileName = $file['name'];   
+    $extension = strtolower(substr($fileName, strpos($fileName, '.')+1)); 
+    $tmp_name = $file['tmp_name'];
+    if(!empty($file)) {
+        $size = $file['size'].'<br>';
+        $maxSize = 10000;
+        $type = $file['type'];
+        // echo $file['tmp_name'].'<br>';
+        // echo $file['error'];
+		$location = 'uploads/';
+		if( (($extension=='jpeg') || ($extension=='jpg')) && $type=='image/jpeg' ) { 
+			if(move_uploaded_file($tmp_name, $location.$fileName)) {
+				// echo 'Uploaded!';
+				return true;
+			} else {
+				echo "Error in uploading";
+				return false;
+			}
+		} else {
+			echo "File must be jpeg, png or jpg.";
+			return false;
+		}
+    }
+}
 
 ?>
 
@@ -52,15 +82,23 @@ function setInterval($year, $month)
     <head>
     </head>
     <body>
-        <form method="POST" action="year_month_form.php">
+        <form method="POST" action="year_month_form.php" enctype="multipart/form-data">
             Year :<br>
             <input type="text" name="year"><br><br>
             Month :<br>
             <input type="text" name="month"><br><br>
+            <input type="file" name="image"><br><br>
             <input type="submit" name="showCalendar">
         </form>
         <hr>
-        <table border="1">
+        <?php 
+            if(isset($_SESSION['image']) && isset($_SESSION['path']))
+            {
+                echo '<img src="'.$_SESSION['path'].$_SESSION['image'].'"';
+            }
+        ?>
+        <div>
+        <table border="1" background = "<?php echo $_SESSION['path'].$_SESSION['image']?>">
             <tr>
                 <th>Sun</th>
                 <th>Mon</th>
@@ -100,5 +138,6 @@ function setInterval($year, $month)
                }
             ?> 
         </table>
+        </div>
     </body>
 </html>
